5c8bc1563d6bafae7be8c26d175750eb4fe3244f
Irenyak1 <inyakate@gmail.com>
2019-07-16 16:50:25 +0300

    add four numbers in non parameter function

---
+++ irene.js
@@ -0,0 +1,10 @@
+function addFourNumbers(){
+    var number1 = 20;
+    var number2 = 40;
+    var number3 = 70;
+    var number4 = 34;
+    var total = number1 + number2 + number3 + number4;
+    
+    console.log("The total is: " + total);
+    }
+    addFourNumbers();
