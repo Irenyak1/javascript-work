57b56050d36df97880ab2c3da6a19e083de4297f
Irenyak1 <inyakate@gmail.com>
2019-07-17 16:25:07 +0300

    Edited comments

--- irene.js
+++ irene.js
@@ -5,7 +5,6 @@ total, with declared variables and assigned values respectively
 */
 function addFourNumbers()
 {
-    
     var number1 = 20;
     var number2 = 40;
     var number3 = 70;
@@ -16,8 +15,7 @@ function addFourNumbers()
     
     }
     // calling the function "addFourNumbers" which adds the four variables
-    addFourNumbers();
-    
+    addFourNumbers(); 
 
 /* 
 A four parameterized function to add four numbers
@@ -32,7 +30,6 @@ function addFourNumberWithParameters(numberOne, numberTwo, numberThree, numberFo
     // calling the function "addFourNumberWithParameters" which adds the four parameters passed
     addFourNumberWithParameters(7, 8, 9, 90);
     
-
 /* 
 A function to print odd numbers between 50 and 100 with a for loop
 iterating between 50 and 100 and an if statement to output odd
@@ -50,4 +47,4 @@ function printOddNumbers()
     }
     // calling the function "printOddNumbers" which prints odd numbers between 50 and 100
     printOddNumbers();
-
+    
